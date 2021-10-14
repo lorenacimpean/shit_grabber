a9c77c2b32b54700e9340abfd3c6cc3dc8e158b5
lorenacimpean <lorenacimpean@gmail.com>
2021-10-14 13:56:50 +0300

    added app colors

--- lib/themes/app_colors.dart
+++ lib/themes/app_colors.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/cupertino.dart';
 
 class AppColors {
-  static const Color primary = Color(0xFFF30070);
+  static const Color primary = Color(0xFFF27A54);
+  static const Color secondary = Color(0xFFA154F2);
+  static const Color tertiary = Color(0xFF6FCF97);
+  static const Color lightGrey = Color(0xFFF2F4F7);
+  static const Color grey = Color(0xFFCBCFD4);
+  static const Color darkGrey = Color(0xFF3C444C);
+  static const Color appBlack = Color(0xFF30363D);
 }
