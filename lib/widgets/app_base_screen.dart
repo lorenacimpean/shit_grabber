3010c619ac3d141a40aea1e386546a68df7b62a5
lorenacimpean <lorenacimpean@gmail.com>
2021-10-15 05:25:48 +0300

    finished bottom navigation bar

--- lib/widgets/app_base_screen.dart
+++ lib/widgets/app_base_screen.dart
@@ -3,11 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shit_grabber/controllers/base_controlles.dart';
 import 'package:shit_grabber/themes/app_colors.dart';
-import 'package:shit_grabber/themes/app_dimensions.dart';
-import 'package:shit_grabber/themes/app_icon_paths.dart';
-import 'package:shit_grabber/themes/app_strings.dart';
 import 'package:shit_grabber/themes/app_text_theme.dart';
-import 'package:shit_grabber/widgets/gradient_text_widget.dart';
+import 'package:shit_grabber/widgets/app_bottom_nav_widget.dart';
+import 'package:shit_grabber/widgets/gradient_widget.dart';
 
 class AppBaseScreen extends GetView<BaseController> {
   final String screenTitle;
@@ -25,59 +23,23 @@ class AppBaseScreen extends GetView<BaseController> {
       builder: (controller) => Scaffold(
         appBar: _buildAppBar,
         body: _body,
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: controller.selectedIndex,
-          onTap: controller.selectNavigationItem,
-          unselectedItemColor: AppColors.secondary,
-          showSelectedLabels: false,
-          enableFeedback: true,
-          type: BottomNavigationBarType.shifting,
-          items: _navigationItems(),
+        bottomNavigationBar: AppBottomNavigationWidget(
+          selectedIndex: controller.selectedIndex,
+          onTabSelected: controller.selectNavigationItem,
         ),
       ),
     );
   }
 
   AppBar get _buildAppBar => AppBar(
-        title: GradientText(
+        title: GradientWidget(Text(
           screenTitle,
           style: AppTextTheme.darkTextTheme.headline2
               ?.copyWith(letterSpacing: 2.0),
-        ),
+        )),
         elevation: 0.0,
         backgroundColor: AppColors.appBlack,
       );
 
   Widget get _body => SafeArea(child: body);
-
-  List<BottomNavigationBarItem> _navigationItems() => [
-        const BottomNavigationBarItem(
-          icon: Image(
-            image: AssetImage(AppIconPaths.home),
-            height: AppDimensions.defaultPadding,
-          ),
-          label: AppStrings.dashboard,
-        ),
-        const BottomNavigationBarItem(
-          icon: Image(
-            image: AssetImage(AppIconPaths.link),
-            height: AppDimensions.defaultPadding,
-          ),
-          label: AppStrings.loadDocuments,
-        ),
-        const BottomNavigationBarItem(
-          icon: Image(
-            image: AssetImage(AppIconPaths.phoneLink),
-            height: AppDimensions.defaultPadding,
-          ),
-          label: AppStrings.takePhoto,
-        ),
-        const BottomNavigationBarItem(
-          icon: Image(
-            image: AssetImage(AppIconPaths.settings),
-            height: AppDimensions.defaultPadding,
-          ),
-          label: AppStrings.settings,
-        ),
-      ];
 }
--- lib/widgets/gradient_widget.dart
+++ lib/widgets/gradient_widget.dart
@@ -3,14 +3,10 @@ import 'package:shit_grabber/themes/app_colors.dart';
 
 //TODO: transform this into gradient mask that takes any child and use it for icons
 
-class GradientText extends StatelessWidget {
-  final String text;
-  final TextStyle? style;
+class GradientWidget extends StatelessWidget {
+  final Widget child;
 
-  const GradientText(
-    this.text, {
-    this.style,
-  });
+  const GradientWidget(this.child, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +15,7 @@ class GradientText extends StatelessWidget {
       shaderCallback: (bounds) => AppColors.gradient.createShader(
         Rect.fromLTWH(0, 0, bounds.width, bounds.height),
       ),
-      child: Text(text, style: style),
+      child: child,
     );
   }
 }
