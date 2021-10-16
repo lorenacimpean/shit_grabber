005c645972bc9a05df2520aa90a0fddab322befd
lorenacimpean <lorenacimpean@gmail.com>
2021-10-16 15:27:17 +0300

    changed font

--- lib/themes/app_text_theme.dart
+++ lib/themes/app_text_theme.dart
@@ -7,35 +7,35 @@ import 'package:shit_grabber/themes/app_dimensions.dart';
 class AppTextTheme {
   static TextTheme lightTextTheme = const TextTheme(
     bodyText1: TextStyle(
-      fontFamily: 'SpaceMono',
+      fontFamily: 'Roboto',
       fontSize: AppDimensions.bodyTextSize,
       fontStyle: FontStyle.normal,
       fontWeight: FontWeight.normal,
       color: AppColors.appBlack,
     ),
     headline1: TextStyle(
-      fontFamily: 'SpaceMono',
+      fontFamily: 'Roboto',
       fontSize: AppDimensions.headline1Size,
       fontStyle: FontStyle.normal,
       fontWeight: FontWeight.bold,
       color: AppColors.appBlack,
     ),
     headline2: TextStyle(
-      fontFamily: 'SpaceMono',
+      fontFamily: 'Roboto',
       fontSize: AppDimensions.headline2Size,
       fontStyle: FontStyle.normal,
       fontWeight: FontWeight.w700,
       color: AppColors.appBlack,
     ),
     headline3: TextStyle(
-      fontFamily: 'SpaceMono',
+      fontFamily: 'Roboto',
       fontSize: AppDimensions.headline3Size,
       fontStyle: FontStyle.normal,
       fontWeight: FontWeight.w600,
       color: AppColors.appBlack,
     ),
     headline4: TextStyle(
-      fontFamily: 'SpaceMono',
+      fontFamily: 'Roboto',
       fontSize: AppDimensions.headline4Size,
       fontStyle: FontStyle.normal,
       fontWeight: FontWeight.normal,
@@ -45,35 +45,35 @@ class AppTextTheme {
 
   static TextTheme darkTextTheme = const TextTheme(
     bodyText1: TextStyle(
-      fontFamily: 'SpaceMono',
+      fontFamily: 'Roboto',
       fontSize: AppDimensions.bodyTextSize,
       fontStyle: FontStyle.normal,
       fontWeight: FontWeight.normal,
       color: AppColors.appWhite,
     ),
     headline1: TextStyle(
-      fontFamily: 'SpaceMono',
+      fontFamily: 'Roboto',
       fontSize: AppDimensions.headline1Size,
       fontStyle: FontStyle.normal,
       fontWeight: FontWeight.bold,
       color: AppColors.appWhite,
     ),
     headline2: TextStyle(
-      fontFamily: 'SpaceMono',
+      fontFamily: 'Roboto',
       fontSize: AppDimensions.headline2Size,
       fontStyle: FontStyle.normal,
       fontWeight: FontWeight.w700,
       color: AppColors.appWhite,
     ),
     headline3: TextStyle(
-      fontFamily: 'SpaceMono',
+      fontFamily: 'Roboto',
       fontSize: AppDimensions.headline3Size,
       fontStyle: FontStyle.normal,
       fontWeight: FontWeight.w600,
       color: AppColors.appWhite,
     ),
     headline4: TextStyle(
-      fontFamily: 'SpaceMono',
+      fontFamily: 'Roboto',
       fontSize: AppDimensions.headline4Size,
       fontStyle: FontStyle.normal,
       fontWeight: FontWeight.normal,
